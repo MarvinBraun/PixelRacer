6c3ab893bf2ec887b444e47b0a80141567b52a8a
MarvinBraun <34036123+MarvinBraun@users.noreply.github.com>
2017-11-30 17:43:22 +0100

    #DB

--- src/Fahrt/Fahrtverwaltung.java
+++ src/Fahrt/Fahrtverwaltung.java
@@ -4,6 +4,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 
+import Datenbankverwaltung.Datenbankschnittstelle;
+
 public class Fahrtverwaltung {
 	
 	HashSet<SingleplayerFahrt> singleplayerFahrten;
@@ -11,15 +13,40 @@ public class Fahrtverwaltung {
 	
 	public Fahrtverwaltung(ResultSet rs)
 	{
+		
+	}
+	
+	public HashSet gibSingleplayerFahrten()
+	{
+		ResultSet rs = Datenbankschnittstelle.abfrage("select * from Singleplayerfahrt");
 		try {
 			while(rs.next())
 			{
 				
 			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return singleplayerFahrten;
 	}
+	
+	public HashSet gibMultiplayerFahrten()
+	{
+		ResultSet rs = Datenbankschnittstelle.abfrage("select * from Multiplayerfahrt");
+		try {
+			while(rs.next())
+			{
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return multiplayerFahrten;
+	}
+	
 	
 }
