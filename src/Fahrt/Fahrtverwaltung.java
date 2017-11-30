b3e84d076bd013b0e3c204e84686ad14e4a403e0
MarvinBraun <34036123+MarvinBraun@users.noreply.github.com>
2017-11-30 17:31:36 +0100

    Fahrtverwaltung

--- src/Fahrt/Fahrtverwaltung.java
+++ src/Fahrt/Fahrtverwaltung.java
@@ -1,5 +1,25 @@
 package Fahrt;
 
-public class Fahrtverwaltung {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
 
+public class Fahrtverwaltung {
+	
+	HashSet<SingleplayerFahrt> singleplayerFahrten;
+	HashSet<MultiplayerFahrt> multiplayerFahrten;
+	
+	public Fahrtverwaltung(ResultSet rs)
+	{
+		try {
+			while(rs.next())
+			{
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
