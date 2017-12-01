f20e53a5b50387a1be4d0fcd444f9e811060f0b1
Adolf <Adolf@Adolf-PC>
2017-12-01 17:56:11 +0100

    master

--- src/Kart/Kart.java
+++ src/Kart/Kart.java
@@ -1,5 +1,32 @@
 package Kart;
 
 public class Kart {
+	public String kartname;
+	public int beschleunigung;
+	public int geschwindigkeit;
+
+	public void setKartname(String kartname) {
+		this.kartname = kartname;
+	}
+
+	public void setBeschleunigung(int beschleunigung) {
+		this.beschleunigung = beschleunigung;
+	}
+
+	public void setGeschwindigkeit(int geschwindigkeit) {
+		this.geschwindigkeit = geschwindigkeit;
+	}
+
+	public String getKartname() {
+		return kartname;
+	}
+
+	public int getBeschleunigung() {
+		return beschleunigung;
+	}
+
+	public int getGeschwindigkeit() {
+		return geschwindigkeit;
+	}
 
 }
--- src/Kart/Kartverwaltung.java
+++ src/Kart/Kartverwaltung.java
@@ -2,19 +2,6 @@ package Kart;
 
 
 public class Kartverwaltung {
-	public String kartname;
-	public int beschleunigung;
-	public int geschwindigkeit;
-
-	public String getKartname() {
-		return kartname;
+	
 	}
 
-	public int getBeschleunigung() {
-		return beschleunigung;
-	}
-
-	public int getGeschwindigkeit() {
-		return geschwindigkeit;
-	}
-}
