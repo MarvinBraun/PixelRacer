dd10c92ecd3ce2b16770c7a06f6a9377f8de1d4d
Adolf <Adolf@Adolf-PC>
2017-12-01 20:16:04 +0100

    master

--- src/Datenbankverwaltung/Datenbankschnittstelle.java
+++ src/Datenbankverwaltung/Datenbankschnittstelle.java
@@ -9,96 +9,80 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Datenbankschnittstelle {
-	
+
 	private static Connection con;
 
-	
-	public static Connection getConnection()
-	{
+	public static Connection getConnection() {
 		try {
-			
-		con = DriverManager.getConnection("jdbc:oracle:thin:@aix1.fh-bielefeld.de:1521:d2","dvi511","fh5902");
-		con.setAutoCommit(false);
-			}
-	
-	catch (SQLException e)
-		{
-		System.out.println("Verbindung zur Datenbank fehlgeschlagen"+"Fehler: " + e.getMessage());
+
+			con = DriverManager.getConnection("jdbc:oracle:thin:@aix1.fh-bielefeld.de:1521:d2", "dvi511", "fh5902");
+			con.setAutoCommit(false);
+		}
+
+		catch (SQLException e) {
+			System.out.println("Verbindung zur Datenbank fehlgeschlagen" + "Fehler: " + e.getMessage());
 		}
 		return con;
 	}
-	
-	
-	public static ResultSet executeQuery(String s)
-	{	
+
+	public static ResultSet executeQuery(String s) {
 		ResultSet rs = null;
 		try {
-		con = getConnection();
-		Statement stmt = con.createStatement();
-		rs = stmt.executeQuery(s);
-	
+			con = getConnection();
+			Statement stmt = con.createStatement();
+			rs = stmt.executeQuery(s);
+
 			stmt.close();
 			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			System.out.println("Abfrage konnte nicht durchgeführt werden. "+"Fehler: " + e.getMessage());
+			System.out.println("Abfrage konnte nicht durchgeführt werden. " + "Fehler: " + e.getMessage());
 		}
-		
+
 		return rs;
 	}
-	
-	
-	public static void executeUpdate(String s)
-	{
+
+	public static void executeUpdate(String s) {
 		int updateInfo = -1;
 		try {
-			
+
 			con = getConnection();
 			Statement stmt = con.createStatement();
-			updateInfo =stmt.executeUpdate(s);
+			updateInfo = stmt.executeUpdate(s);
 			con.commit();
 			stmt.close();
-			
+
 		}
-		
-		catch(SQLException e)
-		{
-			System.out.println("Update konnte nicht durchgeführt werden. Rollback durchgeführt. "+"Fehler: " + e.getMessage());
-			
+
+		catch (SQLException e) {
+			System.out.println(
+					"Update konnte nicht durchgeführt werden. Rollback durchgeführt. " + "Fehler: " + e.getMessage());
+
+			// TODO Auto-generated catch block
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					System.out.println("Rollback fehlgeschlagen");
-				}
-				e.printStackTrace();
+				System.out.println("Rollback fehlgeschlagen");
 			}
+			e.printStackTrace();
 		}
-	
-	public static void executeBatch(String queries[])
-	{
-		try {
-		con = getConnection();
-		Statement stmt = con.createStatement();
-		for(String query : queries)
-		{
-			stmt.addBatch(query);
-		}
-		stmt.executeBatch();
-		stmt.close();
-		con.close();
-		}
-		catch(SQLException e)
-		{
-			System.out.println("Batch konnte nicht ausgeführt werden. "+"Fehler: " + e.getMessage());
-		}
-		
 	}
-	
+
+	public static void executeBatch(String queries[]) {
+		try {
+			con = getConnection();
+			Statement stmt = con.createStatement();
+			for (String query : queries) {
+				stmt.addBatch(query);
+			}
+			stmt.executeBatch();
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Batch konnte nicht ausgeführt werden. " + "Fehler: " + e.getMessage());
+		}
+
+	}
+
 }
-
-	
-	
-
-
--- src/Kart/Kartverwaltung.java
+++ src/Kart/Kartverwaltung.java
@@ -1,8 +1,49 @@
 package Kart;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+
+
+
 
 public class Kartverwaltung {
-	
-	
-	}
 
+	LinkedList<Kartverwaltung> ll;
+	private static Connection con;
+
+	public static Connection getConnection() {
+		try {
+
+			con = DriverManager.getConnection("jdbc:oracle:thin:@aix1.fh-bielefeld.de:1521:d2", "dvi511", "fh5902");
+			con.setAutoCommit(false);
+		} catch (SQLException e) {
+			System.out.println("Verbindung zur Datenbank fehlgeschlagen" + "Fehler: " + e.getMessage());
+		}
+		return con;
+	}
+	// Ich hab ne DB verbindeung eingebaut, damit ich es erstmal ausprobieren kann
+
+	public LinkedList<Kartverwaltung> gibKart() {
+
+		String abfrage = "select * from kart;";
+
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(abfrage);
+			rs.next();
+			ll.add((Kartverwaltung) rs);
+			
+			rs.close();
+			stmt.close();
+			con.close();
+			
+		} catch (SQLException sql) {
+			System.out.println("Fehler beim auslesend er Karts" + sql.getMessage());
+		}
+		return ll;
+	}
+}
