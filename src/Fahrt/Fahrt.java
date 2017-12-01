6a8645be88a9e36459d6cac49fdfcb7e50b60638
MarvinBraun <34036123+MarvinBraun@users.noreply.github.com>
2017-12-01 16:24:39 +0100

    added getter&setter

--- src/Fahrt/Fahrt.java
+++ src/Fahrt/Fahrt.java
@@ -1,10 +1,43 @@
 package Fahrt;
 
-public class Fahrt {
-	int sitzungsID;
-	int rang;
-	float zeit;
-	String Strecke;
-	String kart;
+public abstract class Fahrt {
+	private int sitzungsID;
+	private int rang;
+	private float zeit;
+	private String Strecke;
+	private String kart;
+	
+	public int getSitzungsID() {
+		return sitzungsID;
+	}
+	public void setSitzungsID(int sitzungsID) {
+		this.sitzungsID = sitzungsID;
+	}
+	public int getRang() {
+		return rang;
+	}
+	public void setRang(int rang) {
+		this.rang = rang;
+	}
+	public float getZeit() {
+		return zeit;
+	}
+	public void setZeit(float zeit) {
+		this.zeit = zeit;
+	}
+	public String getStrecke() {
+		return Strecke;
+	}
+	public void setStrecke(String strecke) {
+		Strecke = strecke;
+	}
+	public String getKart() {
+		return kart;
+	}
+	public void setKart(String kart) {
+		this.kart = kart;
+	}
+	
+	
 	
 }
