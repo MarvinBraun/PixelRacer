09deea5d680adb1e433800262ed5ed316fbe794b
MarvinBraun <34036123+MarvinBraun@users.noreply.github.com>
2017-12-01 10:40:58 +0100

    #dbtest

--- src/Datenbankverwaltung/Datenbankschnittstelle.java
+++ src/Datenbankverwaltung/Datenbankschnittstelle.java
@@ -1,5 +1,14 @@
 package Datenbankverwaltung;
 
-public class DBAbfrage {
+import java.sql.ResultSet;
+
+public class Datenbankschnittstelle {
+	
+	public static ResultSet abfrage(String s)
+	{
+		ResultSet rs = null;
+	
+		return rs;
+	}
 
 }
