2c79855c0d7cb06ec88fa21752c9f56119ad52d3
Adolf <Adolf@Adolf-PC>
2017-12-01 17:51:35 +0100

    master

--- src/Kart/Kartverwaltung.java
+++ src/Kart/Kartverwaltung.java
@@ -1,5 +1,20 @@
 package Kart;
 
+
 public class Kartverwaltung {
 	public String kartname;
+	public int beschleunigung;
+	public int geschwindigkeit;
+
+	public String getKartname() {
+		return kartname;
+	}
+
+	public int getBeschleunigung() {
+		return beschleunigung;
+	}
+
+	public int getGeschwindigkeit() {
+		return geschwindigkeit;
+	}
 }
