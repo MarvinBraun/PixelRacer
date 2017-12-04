beb2ae7073efaba11c3d3da5b044160c95916bae
Adolf <Adolf@Adolf-PC>
2017-12-04 18:40:20 +0100

    Ansichten für die Premium gegenstände erstellt

---
+++ src/Premium/PremiumStreckeView.java
@@ -0,0 +1,81 @@
+package Premium;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class PremiumStreckeView {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PremiumStreckeView window = new PremiumStreckeView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PremiumStreckeView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 432, 151);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{182, 38, 194, 0};
+		gridBagLayout.rowHeights = new int[]{30, 0, 23, 23, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JLabel Frage = new JLabel("  M\u00F6chten Sie die Strecke wirklich f\u00FCr 4,99\u20AC erwerben?");
+		GridBagConstraints gbc_Frage = new GridBagConstraints();
+		gbc_Frage.anchor = GridBagConstraints.NORTH;
+		gbc_Frage.insets = new Insets(0, 0, 5, 0);
+		gbc_Frage.gridwidth = 3;
+		gbc_Frage.gridx = 0;
+		gbc_Frage.gridy = 1;
+		frame.getContentPane().add(Frage, gbc_Frage);
+		Frage.setFont(new Font("Calibri", Font.PLAIN, 18));
+		
+		JButton bestätigen = new JButton("Best\u00E4tigen");
+		GridBagConstraints gbc_bestätigen = new GridBagConstraints();
+		gbc_bestätigen.anchor = GridBagConstraints.NORTHEAST;
+		gbc_bestätigen.insets = new Insets(0, 0, 5, 5);
+		gbc_bestätigen.gridx = 0;
+		gbc_bestätigen.gridy = 2;
+		frame.getContentPane().add(bestätigen, gbc_bestätigen);
+		
+		JButton abbrechen = new JButton("Abbrechen");
+		GridBagConstraints gbc_abbrechen = new GridBagConstraints();
+		gbc_abbrechen.insets = new Insets(0, 0, 5, 0);
+		gbc_abbrechen.anchor = GridBagConstraints.NORTHWEST;
+		gbc_abbrechen.gridx = 2;
+		gbc_abbrechen.gridy = 2;
+		frame.getContentPane().add(abbrechen, gbc_abbrechen);
+	}
+
+}
