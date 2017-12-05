79dfdeed5a85dd1f93a08d59a1b0972b40de8aba
MarvinBraun <34036123+MarvinBraun@users.noreply.github.com>
2017-12-05 21:43:32 +0100

    #DB

--- src/Datenbankverwaltung/Datenbankschnittstelle.java
+++ src/Datenbankverwaltung/Datenbankschnittstelle.java
@@ -2,15 +2,35 @@
 
 package Datenbankverwaltung;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.imageio.ImageIO;
+import javax.sql.rowset.serial.SerialException;
+
 public class Datenbankschnittstelle {
 
 	private static Connection con;
+	
+	public static void main(String[] args)
+	{
+		ResultSet rs = executeQuery("Select * from Kunde");
+		
+		
+		executeUpdate("insert into Mitarbeiter values (12345648,'Tester12','testest','Max','Mustermann','CEO','Maxddd@gmx.de','03.04.94')");	
+		System.out.println("durchgeführt");
+		executeUpdate("insert into kart values ('FireBird','100','300','false',0,null)");	
+	}
 
 	public static Connection getConnection() {
 		try {
@@ -51,6 +71,7 @@ public class Datenbankschnittstelle {
 			updateInfo = stmt.executeUpdate(s);
 			con.commit();
 			stmt.close();
+			con.close();
 
 		}
 
@@ -83,6 +104,63 @@ public class Datenbankschnittstelle {
 			System.out.println("Batch konnte nicht ausgeführt werden. " + "Fehler: " + e.getMessage());
 		}
 
+	}
+	
+	public static Blob konvertiereBild(BufferedImage bild)
+	{
+		Blob blFile = null;
+		try {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(bild, "png", baos);
+		 blFile = new javax.sql.rowset.serial.SerialBlob(baos.toByteArray());
+		}
+		catch(Exception e)
+		{
+			System.out.println("Fehler!");
+			e.printStackTrace();
+			
+		}
+		
+		return blFile;
+		
+	}
+	
+	public static void sendImage()
+	{
+		String s = "insert into kart values ('FireBird','100','300','false',0,null)";
+		String sql = "update kart set grafik=? " + " where kartname ='FireBird'";
+				Connection con;
+		con = getConnection();
+				//try {
+					executeUpdate(s);
+					/*PreparedStatement myStmt = con.prepareStatement(sql);
+					File f =new File("src/Resources/Car1.png");
+					FileInputStream input = new FileInputStream(f);
+					myStmt.setBinaryStream(1, input);
+					myStmt.executeUpdate();
+					System.out.println("Successfull");
+				*/
+					
+				} 
+				/*
+				catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				
+				}
+				
+				
+				catch (IOException f)
+				{
+					System.out.println("Bildfehler");
+				}
+			
+				
+	}
+		*/
+	public static void receiveKart()
+	{
+		
 	}
 
 }
--- src/Datenbankverwaltung/Testklasse.java
+++ src/Datenbankverwaltung/Testklasse.java
@@ -10,5 +10,12 @@ public class Testklasse {
 		Datenbankschnittstelle.executeUpdate(s);
 	}
 	
+	public static void sendeKart()
+	{
+		BufferedImage bild = new BufferedImage(new File("src/Resources/Car1.png"));
+		Blob b  = Datenbankschnittstelle.konvertiereBild(bild);
+		String s = "insert data into kart values ('FireBird','100','300','false',0,
+	}
+	
 
 }
