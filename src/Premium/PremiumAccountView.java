3b214622bf7ba9cf988656354000c7f3ee11c1b6
TH3_H4CK3R_1337 <TH3_H4CK3R_1337@DESKTOP-MB1QBH9.dhcp.fh-bielefeld.de>
2017-12-07 09:52:55 +0100

    actionlistener eingefügt

--- src/Premium/PremiumAccountView.java
+++ src/Premium/PremiumAccountView.java
@@ -4,19 +4,25 @@ package Premium;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+
 import javax.swing.JLabel;
 import java.awt.Font;
-import javax.swing.SwingConstants;
+
+import Rechnung.anzeigenRechnung_ansicht2;
+
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PremiumAccountView {
+public class PremiumAccountView extends JFrame implements ActionListener {
 
 	private JFrame frame;
+	JButton bestätigen = new JButton("Bestätigen");
+	JButton abbrechen = new JButton("Abbrechen");
+	anzeigenRechnung_ansicht2 rechnung = new anzeigenRechnung_ansicht2();
 
 	/**
 	 * Launch the application.
@@ -39,6 +45,8 @@ public class PremiumAccountView {
 	 */
 	public PremiumAccountView() {
 		initialize();
+		bestätigen.addActionListener(this);
+		abbrechen.addActionListener(this);
 	}
 
 	/**
@@ -49,12 +57,12 @@ public class PremiumAccountView {
 		frame.setBounds(100, 100, 432, 151);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{182, 38, 194, 0};
-		gridBagLayout.rowHeights = new int[]{30, 0, 23, 23, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 182, 38, 194, 0 };
+		gridBagLayout.rowHeights = new int[] { 30, 0, 23, 23, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		frame.getContentPane().setLayout(gridBagLayout);
-		
+
 		JLabel Frage = new JLabel("Möchten Sie wirklich für 9,99€ auf Premium upgraden?");
 		GridBagConstraints gbc_Frage = new GridBagConstraints();
 		gbc_Frage.anchor = GridBagConstraints.NORTH;
@@ -64,21 +72,31 @@ public class PremiumAccountView {
 		gbc_Frage.gridy = 1;
 		frame.getContentPane().add(Frage, gbc_Frage);
 		Frage.setFont(new Font("Calibri", Font.PLAIN, 18));
-		
-		JButton bestätigen = new JButton("Bestätigen");
+
 		GridBagConstraints gbc_bestätigen = new GridBagConstraints();
 		gbc_bestätigen.anchor = GridBagConstraints.NORTHEAST;
 		gbc_bestätigen.insets = new Insets(0, 0, 5, 5);
 		gbc_bestätigen.gridx = 0;
 		gbc_bestätigen.gridy = 2;
 		frame.getContentPane().add(bestätigen, gbc_bestätigen);
-		
-		JButton abbrechen = new JButton("Abbrechen");
+
 		GridBagConstraints gbc_abbrechen = new GridBagConstraints();
 		gbc_abbrechen.insets = new Insets(0, 0, 5, 0);
 		gbc_abbrechen.anchor = GridBagConstraints.NORTHWEST;
 		gbc_abbrechen.gridx = 2;
 		gbc_abbrechen.gridy = 2;
 		frame.getContentPane().add(abbrechen, gbc_abbrechen);
+
 	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == bestätigen) {
+			anzeigenRechnung_ansicht2.main(null);
+		}
+		if (e.getSource() == abbrechen) {
+			System.exit(0);
+		}
+
+	}
+
 }
