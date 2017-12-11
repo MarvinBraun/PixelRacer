e5daf1050dfe3072f88efd0936a23ba077e0d2cb
FerhatKoca <34124451+FerhatKoca@users.noreply.github.com>
2017-12-11 12:33:46 +0100

    Update Bot.java

--- src/Computergegner/Bot.java
+++ src/Computergegner/Bot.java
@@ -1,18 +1,30 @@
 //@Author Ferhat Koca
 package Computergegner;
 
+import Strecke.Strecke;
+import Kart.Kart;
+
 public class Bot {
-	//Stecke = gib l‰nge
-	//geschw. = gib strecke
-	//beschl = gib beschl
-	//beschl = geschw/beschl
-	//Strecke = gib schwierigkeit
-	
-	//(Strecke/geschw + beschlZeit)*math.random());
-	int strecke = Strecke.Strecke.getLaenge();
-	int geschw = Kart.Kart.getMaxkmh();
-	int beschl = Kart.Kart.getBeschleunigung();
-	int schw = Strecke.Strecke.getSchwierigkeit();
-	
-	
+
+	static int p;
+	static int strecke = Strecke.getLaenge();
+	static int geschwindigkeit = Kart.getMaxkmh();
+	static int beschleunigung = Kart.getBeschleunigung();
+
+	public Bot() {
+		p = (int) (((strecke / geschwindigkeit) + (geschwindigkeit / beschleunigung)) * Math.random());
+	}
+
+	public static void main(String[] args) {
+		Bot bot = new Bot();
+		System.out.println(bot);
+	}
+
 }
+// Stecke = gib l√§nge
+// geschw. = gib strecke
+// beschl = gib beschl
+// beschl = geschw/beschl
+// Strecke = gib schwierigkeit
+
+// (Strecke/geschw + beschlZeit)*math.random());
