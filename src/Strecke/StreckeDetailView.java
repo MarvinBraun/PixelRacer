41a4cd76fa34b874e393b0fcde9c7f7d88f1befa
rdemmler <34124377+rdemmler@users.noreply.github.com>
2018-01-11 17:59:28 +0100

    final

--- src/Strecke/StreckeDetailView.java
+++ src/Strecke/StreckeDetailView.java
@@ -2,17 +2,22 @@ package Strecke;
 
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import BackgroundAnimation.Movement;
 import FontHandler.FontHandler;
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
+import java.awt.Color;
 
 /**
  * Grafische Benutzeroberflaeche zur Ansicht der Details und nutzerbezogenen Fahrtdaten einer Strecke. 
@@ -69,76 +74,76 @@ public class StreckeDetailView {
 	 */
 	private void initialize() {
 		
-		Font customFont20f = FontHandler.registriereSchriftart(30f); // Laden der Schriftart durch Klasse FontHandler
+		Font customFont20f = FontHandler.registriereSchriftart(10f); // Laden der Schriftart durch Klasse FontHandler
 				
 		setFrame(new JFrame());
 		getFrame().setBounds(100, 100, 450, 300);
-		getFrame().setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		getFrame().setDefaultCloseOperation(getFrame().DO_NOTHING_ON_CLOSE);
 		getFrame().setVisible(false);
 		getFrame().setResizable(false);
 		getFrame().getContentPane().setLayout(null);
 				
-		setLblLaenge(new JLabel("Streckenl\u00E4nge:"));
-		getLblLaenge().setBounds(76, 32, 105, 14);
+		setLblLaenge(new JLabel("Streckenlaenge:"));
+		getLblLaenge().setBounds(76, 32, 118, 14);
 		getLblLaenge().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblLaenge());
-		
+				
 		setLblGefahreneRennen(new JLabel("Gefahrene SP-Rennen:"));
-		getLblGefahreneRennen().setBounds(37, 57, 144, 14);
+		getLblGefahreneRennen().setBounds(33, 57, 174, 14);
 		getLblGefahreneRennen().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblGefahreneRennen());
 		
 		setLblGefahreneKmGesamt(new JLabel("Gefahrene m SP gesamt:"));
-		getLblGefahreneKmGesamt().setBounds(20, 82, 152, 14);
+		getLblGefahreneKmGesamt().setBounds(20, 82, 174, 14);
 		getLblGefahreneKmGesamt().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblGefahreneKmGesamt());
 		
 		setLblRennenAlsErster(new JLabel("Rennen als Erster abgeschlossen"));
-		getLblRennenAlsErster().setBounds(224, 136, 210, 14);
+		getLblRennenAlsErster().setBounds(153, 132, 265, 14);
 		getLblRennenAlsErster().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblRennenAlsErster());
 		
 		setLblRennenAlsZweiter(new JLabel("Rennen als Zweiter abgeschlossen"));
-		getLblRennenAlsZweiter().setBounds(224, 161, 210, 14);
+		getLblRennenAlsZweiter().setBounds(153, 157, 265, 14);
 		getLblRennenAlsZweiter().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblRennenAlsZweiter());
 		
 		setLblRennenAlsDritter(new JLabel("Rennen als Dritter abgeschlossen"));
-		getLblRennenAlsDritter().setBounds(224, 186, 210, 14);
+		getLblRennenAlsDritter().setBounds(153, 182, 265, 14);
 		getLblRennenAlsDritter().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblRennenAlsDritter());
 		
 		setLblSetLaenge(new JLabel("New label"));
-		getLblSetLaenge().setBounds(191, 32, 46, 14);
+		getLblSetLaenge().setBounds(248, 32, 46, 14);
 		getLblSetLaenge().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblSetLaenge());
 		
 		setLblSetGesRennen(new JLabel("New label"));
-		getLblSetGesRennen().setBounds(191, 57, 46, 14);
+		getLblSetGesRennen().setBounds(248, 57, 46, 14);
 		getLblSetGesRennen().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblSetGesRennen());
 		
 		setLblSetGesKm(new JLabel("New label"));
-		getLblSetGesKm().setBounds(191, 82, 46, 14);
+		getLblSetGesKm().setBounds(248, 82, 46, 14);
 		getLblSetGesKm().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblSetGesKm());
 		
 		setLblSetAnzErster(new JLabel("New label"));
-		getLblSetAnzErster().setBounds(148, 136, 46, 14);
+		getLblSetAnzErster().setBounds(77, 132, 46, 14);
 		getLblSetAnzErster().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblSetAnzErster());
 		
 		setLblSetAnzZweiter(new JLabel("New label"));
-		getLblSetAnzZweiter().setBounds(148, 161, 46, 14);
+		getLblSetAnzZweiter().setBounds(77, 157, 46, 14);
 		getLblSetAnzZweiter().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblSetAnzZweiter());
 		
 		setLblSetAnzDritter(new JLabel("New label"));
-		getLblSetAnzDritter().setBounds(148, 186, 46, 14);
+		getLblSetAnzDritter().setBounds(77, 182, 46, 14);
 		getLblSetAnzDritter().setFont(customFont20f);
 		getFrame().getContentPane().add(getLblSetAnzDritter());
 		
-		setBtnZurueck(new JButton("Zur\u00FCck"));
+		setBtnZurueck(new JButton("Zurueck"));
 		getBtnZurueck().setBounds(10, 227, 89, 23);
 		getBtnZurueck().addActionListener(new ActionListener(){
 
@@ -148,6 +153,10 @@ public class StreckeDetailView {
 			}
 		});
 		getBtnZurueck().setFont(customFont20f);
+		getBtnZurueck().setBorderPainted(false);
+		getBtnZurueck().setOpaque(false);
+		getBtnZurueck().setBackground(SystemColor.activeCaption);
+		getBtnZurueck().setFocusable(false);
 		getFrame().getContentPane().add(getBtnZurueck());
 			
 		setLblHintergrund(new JLabel(""));
@@ -291,5 +300,6 @@ public class StreckeDetailView {
 
 	public void setBtnZurueck(JButton btnZurueck) {
 		this.btnZurueck = btnZurueck;
+		btnZurueck.setForeground(Color.RED);
 	}
 }
