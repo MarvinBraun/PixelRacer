8708136f3baf7818e0800f70cd99e7c7ab2643eb
Sean <Sean@192.168.1.12>
2018-01-12 22:43:42 +0100

    Fertig + JavaDoc

--- src/StreckeHinzufügen/StreckeHinzufügenStrg.java
+++ src/StreckeHinzufügen/StreckeHinzufügenStrg.java
@@ -1,12 +1,14 @@
-/**
- @author Sean Cartner
-*/
-
 package StreckeHinzufügen;
 
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.activation.MimetypesFileTypeMap;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -14,9 +16,16 @@ import Datenbankverwaltung.Datenbankschnittstelle;
 
 import java.awt.event.ActionEvent;
 
+/**
+ * Steuerungsklasse, welche die StreckeHinzufügenView verwaltet, sowie den
+ * Vorgang des Hinzufügen einer neuen Strecke koodiniert.
+ * 
+ * @author Sean
+ *
+ */
 public class StreckeHinzufügenStrg implements ActionListener {
-	
-	//Globae Variablen
+
+	// Globale Variablen
 	private StreckeHinzufügenView stView;
 	private String laengeOk = "true";
 	private String fehlermeldung = "Folgende Angaben sind zu lang:";
@@ -28,9 +37,11 @@ public class StreckeHinzufügenStrg implements ActionListener {
 	private int punktzahl;
 	private char status;
 	private File grafik;
-	
-	//Konstruktor
-	public StreckeHinzufügenStrg(){
+
+	/**
+	 * Konstruktor
+	 */
+	public StreckeHinzufügenStrg() {
 		stView = new StreckeHinzufügenView();
 		stView.getPanel().setVisible(true);
 		stView.getBtnAuswhlen().addActionListener(this);
@@ -38,187 +49,343 @@ public class StreckeHinzufügenStrg implements ActionListener {
 		stView.getBtnAbbrechen().addActionListener(this);
 	}
 
-	//Methode, die auf aufgerufen wird, wenn ein Button gedrueckt wird
+	/**
+	 * Methode, die agiert, wenn ein Button gedrückt wurde. Diese soll, wenn: 
+	 * - BtnAuswaehlen gedrückt wurde den FileChooser aufrufen. 
+	 * - BtnAbsenden gedrückt wurden eine  neue Strecke in der Datenbank erstellen. 
+	 * - BtnAbbrechen gedrückt wurde das Formular leeren.
+	 */
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==stView.getBtnAuswhlen()){
-			//FileChooser aufrufen
+		if (e.getSource() == stView.getBtnAuswhlen()) {
+			// FileChooser aufrufen
 			stView.getFileChooser().setFileFilter(stView.getFilter());
 			int rueckgabeWert = stView.getFileChooser().showOpenDialog(null);
-			if(rueckgabeWert == JFileChooser.APPROVE_OPTION){
+			if (rueckgabeWert == JFileChooser.APPROVE_OPTION) {
 				stView.getTfGrafik().setText(stView.getFileChooser().getSelectedFile().getAbsolutePath());
 			}
-		} 
-		if(e.getSource() == stView.getBtnAbsenden()){
-			//Daten pruefen und wenn alles Korrekt ist einen neuen Datensatz anlegen
-			if(pruefeFormarAufVollständigkeit() == false){
+		}
+		if (e.getSource() == stView.getBtnAbsenden()) {
+			// Daten pruefen und wenn alles Korrekt ist einen neuen Datensatz anlegen
+			if (pruefeFormarAufVollständigkeit() == false) {
 				formularUnvollständigMeldung();
-			} else{
-			istNameZuLang();
-			istLaengeZuLang();
-			istSchwierigkeitZuLang();
-			istPunktzahlZuLang();
-			if(laengeOk == "false"){
-				inhaltZuLangMeldung();
-				fehlermeldung = "Folgende Angaben sind zu lang:";
-				laengeOk = "true";
-			} else{
-				deklariereVariablenVTextfeldern();
-				if(istKeineZahl == true) {
-					istKeineZahlMeldung();
-					//reset
-					istKeineZahl = false;
-				}else {
-					if(!grafik.exists()) {
-						keineDateiGefundenMeldung();
-					}else {
-						if(IstGrafikPng() == false) {
-							nichtPngMeldung();
-						}else {
-							if(grafik.length() > 256000) {
-								grafikZuGrossMeldung();
-							}else {
-								erstelleStreckeInDB();
-								updateKartGrafik();
-								leereFormular();
+			} else {
+				istNameZuLang();
+				istLaengeZuLang();
+				istSchwierigkeitZuLang();
+				istPunktzahlZuLang();
+				if (laengeOk == "false") {
+					inhaltZuLangMeldung();
+					fehlermeldung = "Folgende Angaben sind zu lang:";
+					laengeOk = "true";
+				} else {
+					deklariereVariablenVTextfeldern();
+					if (istKeineZahl == true) {
+						istKeineZahlMeldung();
+						// reset
+						istKeineZahl = false;
+					} else {
+						if (!grafik.exists()) {
+							keineDateiGefundenMeldung();
+						} else {
+							if (IstGrafikPng() == false) {
+								nichtPngMeldung();
+							} else {
+								if (grafik.length() > 256000) {
+									grafikZuGrossMeldung();
+								} else {
+									if (istNameFormatOk() == false) {
+										nameFormatNichtOkMeldung();
+									} else {
+										if (istStreckennameVergeben() == true) {
+											streckenNameBereitsVergebenMeldung();
+										} else {
+											erstelleStreckeInDB();
+											updateStreckenGrafik();
+											speicherStreckeZwischen();
+											streckeErfolgreichAngelegtMeldung();
+											leereFormular();
+										}
+									}
+								}
 							}
 						}
 					}
-				}
 
-			}
 				}
+			}
 		}
-		if(e.getSource() == stView.getBtnAbbrechen()) {
+		if (e.getSource() == stView.getBtnAbbrechen()) {
 			leereFormular();
 		}
 	}
-	
-	//Meldung, die erscheint, wenn Formular nicht vollstaendig ausgefuellt ist
-	private void formularUnvollständigMeldung(){
-		JOptionPane.showMessageDialog(stView.getPanel(),
-			    "Alle Felder müssen ausgefüllt sein!", "Formular unvollständig",
-			    JOptionPane.WARNING_MESSAGE);
+
+	/**
+	 * Meldung, die erscheint, wenn Formular nicht vollstaendig ausgefuellt ist.
+	 */
+	private void formularUnvollständigMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Alle Felder müssen ausgefüllt sein!",
+				"Formular unvollständig", JOptionPane.WARNING_MESSAGE);
 	}
-	
-	//Meldung, die erscheint, wenn die Eingabe zu einem oder mehreren Feldern zu lang ist
-	private void inhaltZuLangMeldung(){
-		JOptionPane.showMessageDialog(stView.getPanel(),
-			    fehlermeldung, "Inhalt zu lang",
-			    JOptionPane.WARNING_MESSAGE);
+
+	/**
+	 * Meldung, die erscheint, wenn die Eingabe zu einem oder mehreren Feldern zu
+	 * lang ist.
+	 */
+	private void inhaltZuLangMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), fehlermeldung, "Inhalt zu lang", JOptionPane.WARNING_MESSAGE);
 	}
-	
-	//Meldung, die erscheint, wenn ein Zahlen-Feld mit sonstigen Zeichen befuellt wird
-	public void istKeineZahlMeldung(){
-		JOptionPane.showMessageDialog(stView.getPanel(),
-			   "Länge, Schwierigkeit und Punktzahl müssen eine Zahl sein!", "Keine Zahl",
-			    JOptionPane.WARNING_MESSAGE);
+
+	/**
+	 * Meldung, die erscheint, wenn ein Zahlen-Feld mit sonstigen Zeichen befuellt
+	 * wird.
+	 */
+	public void istKeineZahlMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Länge, Schwierigkeit und Punktzahl müssen eine Zahl sein!",
+				"Keine Zahl", JOptionPane.WARNING_MESSAGE);
 	}
-	
-	//Meldung, die erscheint, wenn Grafik nicht existiert
-	public void keineDateiGefundenMeldung(){
-		JOptionPane.showMessageDialog(stView.getPanel(),
-			   "Es wurde keine Datei unter dem angegebenen Pfad gefunden", "Keine Datei gefunden",
-			    JOptionPane.WARNING_MESSAGE);
+
+	/**
+	 * Meldung, die erscheint, wenn Grafik nicht existiert.
+	 */
+	public void keineDateiGefundenMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Es wurde keine Datei unter dem angegebenen Pfad gefunden",
+				"Keine Datei gefunden", JOptionPane.WARNING_MESSAGE);
 	}
-	
-	//Meldung, die erscheint, wenn Grafik nicht im png Formart vorliegt
-	public void nichtPngMeldung(){
-		JOptionPane.showMessageDialog(stView.getPanel(),
-			   "Die Grafik muss im Format .png vorliegen", "Grafik liegt nicht im PNG Format vor",
-			    JOptionPane.WARNING_MESSAGE);
+
+	/**
+	 * Meldung, die erscheint, wenn Grafik nicht im png Format vorliegt.
+	 */
+	public void nichtPngMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Die Grafik muss im Format .png vorliegen",
+				"Grafik liegt nicht im PNG Format vor", JOptionPane.WARNING_MESSAGE);
 	}
-	
-	//Meldung, die erscheint, wenn Grafik zu Gross (in Speicher) ist
-	public void grafikZuGrossMeldung(){
-		JOptionPane.showMessageDialog(stView.getPanel(),
-			   "Die Grafik darf maximal 20kb groß sein", "Grafik zu Groß",
-			    JOptionPane.WARNING_MESSAGE);
+
+	/**
+	 * Meldung, die erscheint, wenn Grafik zu Gross (in Speicher) ist.
+	 */
+	public void grafikZuGrossMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Die Grafik darf maximal 20kb groß sein", "Grafik zu Groß",
+				JOptionPane.WARNING_MESSAGE);
 	}
-	
-	//Methode, welche pruefen soll, ob das Formular auch vollstaendig ausgefuellt ist
-	private boolean pruefeFormarAufVollständigkeit(){
-		if(stView.getTfName().getText().isEmpty() || stView.getTfLaenge().getText().isEmpty() || 
-				stView.getTfSchwierigkeit().getText().isEmpty() || stView.getTfPunktzahl().getText().isEmpty() ||
-				stView.getTfPunktzahl().getText().isEmpty() || stView.getBgStatus().getSelection() == null){
-		return false;
+
+	/**
+	 * Meldung, die erscheint, wenn ungueltige Zeichen im Vornamen eingegeben wurde.
+	 */
+	private void nameFormatNichtOkMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(),
+				"Beachte die Groß- und Kleinschreibung beim Namen, sowie die nicht erlaubten Sonderzeichen",
+				"Name ungültig", JOptionPane.WARNING_MESSAGE);
+	}
+
+	/**
+	 * Meldung, die erscheint, wenn der Name aufgrund eines Datenbank-Problems nicht
+	 * kontrolliert werden konnte.
+	 */
+	private void konnteStreckenNamenNichtKontrollierenMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Konnte Streckennamen nicht prüfen", "Datenbankproblem",
+				JOptionPane.WARNING_MESSAGE);
+	}
+
+	/**
+	 * Meldung, die erscheint, wenn der ausgewählte Name bereits vergeben ist.
+	 */
+	private void streckenNameBereitsVergebenMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Der eingegebene Streckenname wird bereits genutzt",
+				"Streckenname vergeben", JOptionPane.WARNING_MESSAGE);
+	}
+
+	/**
+	 * Meldung, die erscheint, wenn eine Strecke zum Zwecke der Zwischenspeicherung
+	 * nicht in eine Datei geschrieben werden konnte.
+	 */
+	private void streckeKonnteNichtZwischengespeichertWerdenMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Strecke konnte nicht zwischengespeichert werden",
+				"Zwischenspeichern nicht möglich", JOptionPane.WARNING_MESSAGE);
+	}
+
+	/**
+	 * Meldung, die erscheint, wenn die Strecke erfolgreich angelegt wurde.
+	 */
+	private void streckeErfolgreichAngelegtMeldung() {
+		JOptionPane.showMessageDialog(stView.getPanel(), "Strecke erfolgreich angelegt", "Erfolgreich",
+				JOptionPane.WARNING_MESSAGE);
+	}
+
+	/**
+	 * Methode, welche pruefen soll, ob das Formular auch vollstaendig ausgefuellt
+	 * ist.
+	 * 
+	 * @return Boolean
+	 */
+	private boolean pruefeFormarAufVollständigkeit() {
+		if (stView.getTfName().getText().isEmpty() || stView.getTfLaenge().getText().isEmpty()
+				|| stView.getTfSchwierigkeit().getText().isEmpty() || stView.getTfPunktzahl().getText().isEmpty()
+				|| stView.getTfPunktzahl().getText().isEmpty() || stView.getBgStatus().getSelection() == null) {
+			return false;
 		}
 		return true;
 	}
-	
-	//Methode, welche die Laenge des Namen-Feldes kontrollieren soll
-	private void istNameZuLang(){
-		if(stView.getTfName().getText().length() > 20){
+
+	/**
+	 * Methode, welche die Laenge des Namen-Feldes kontrollieren soll.
+	 */
+	private void istNameZuLang() {
+		if (stView.getTfName().getText().length() > 20) {
 			laengeOk = "false";
 			fehlermeldung = fehlermeldung + "  " + "Name";
-		}	
+		}
 	}
-	
-	//Methode, welche die Laenge des Länge-Feldes kontrollieren soll
-	private void istLaengeZuLang(){
-		if(stView.getTfLaenge().getText().length() > 5){
+
+	/**
+	 * Methode, welche die Laenge des Länge-Feldes kontrollieren soll.
+	 */
+	private void istLaengeZuLang() {
+		if (stView.getTfLaenge().getText().length() > 5) {
 			laengeOk = "false";
 			fehlermeldung = fehlermeldung + "  " + "Länge";
 		}
 	}
-	
-	//Methode, welche die Laenge des Schwierigkeit-Feldes kontrollieren soll
-	private void istSchwierigkeitZuLang(){
-		if(stView.getTfSchwierigkeit().getText().length() > 1){
+
+	/**
+	 * Methode, welche die Laenge des Schwierigkeit-Feldes kontrollieren soll.
+	 */
+	private void istSchwierigkeitZuLang() {
+		if (stView.getTfSchwierigkeit().getText().length() > 1) {
 			laengeOk = "false";
 			fehlermeldung = fehlermeldung + "  " + "Schwierigkeit";
 		}
 	}
-	
-	//Methode, welche die Laenge des Punktzahl-Feldes kontrollieren soll
-	private void istPunktzahlZuLang(){
-		if(stView.getTfPunktzahl().getText().length() > 2){
+
+	/**
+	 * Methode, welche die Laenge des Punktzahl-Feldes kontrollieren soll.
+	 */
+	private void istPunktzahlZuLang() {
+		if (stView.getTfPunktzahl().getText().length() > 2) {
 			laengeOk = "false";
 			fehlermeldung = fehlermeldung + "  " + "Punktzahl";
 		}
 	}
-	
-	//Methode, die kontrolliert, ob die Grafik im png Format vorliegt
-	private boolean IstGrafikPng(){    
+
+	/**
+	 * Methode, die kontrolliert, ob die Grafik im png Format vorliegt.
+	 * 
+	 * @return Boolean
+	 */
+	private boolean IstGrafikPng() {
 		MimetypesFileTypeMap mtftp = new MimetypesFileTypeMap();
-	    mtftp.addMimeTypes("image png");
-	    String mimetype = mtftp.getContentType(grafik);
-	    String fileType = mimetype.split("/")[0];
-	    return fileType.equalsIgnoreCase(mimetype);
+		mtftp.addMimeTypes("image png");
+		String mimetype = mtftp.getContentType(grafik);
+		String fileType = mimetype.split("/")[0];
+		return fileType.equalsIgnoreCase(mimetype);
 	}
-	
-	//Methode, in die Datenbank relevanten Variablen aufgrundlage des Formulars deklariert werden
-	private void deklariereVariablenVTextfeldern(){
+
+	/**
+	 * Methode, die kontrolliert, ob der Name im richtigen Format eingegeben wurde.
+	 * 
+	 * @return Boolean
+	 */
+	private boolean istNameFormatOk() {
+		Pattern patt = Pattern.compile("[A-ZÄÖÜ][a-zäöüß]+[0-9]{0,4}?([ -]?[A-ZÄÖÜ][a-zäöüß]+)?[0-9]{0,4}");
+		Matcher match = patt.matcher(stView.getTfName().getText());
+		return match.matches();
+	}
+
+	/**
+	 * Methode, die kontrollieren soll, ob der eingegebene Streckenname bereits
+	 * vergeben ist,
+	 * 
+	 * @return
+	 */
+	private boolean istStreckennameVergeben() {
+		ResultSet rs = Datenbankschnittstelle
+				.executeQuery("select streckenname from strecke where streckenname = '" + name + "'");
+		String check = null;
 		try {
-		name = stView.getTfName().getText();
-		laenge = Integer.parseInt(stView.getTfLaenge().getText());
-		schwierigkeit = Integer.parseInt(stView.getTfSchwierigkeit().getText());
-		punktzahl = Integer.parseInt(stView.getTfPunktzahl().getText());
-		status = stView.getRbFree().isSelected()?'f':(stView.getRbPremium().isSelected()?'p':' ');
-		grafik = new File(stView.getTfGrafik().getText());
+			while (rs.next()) {
+				check = rs.getString(1);
+			}
+			rs.close();
+
+		} catch (SQLException e) {
+			konnteStreckenNamenNichtKontrollierenMeldung();
+		}
+		Datenbankschnittstelle.closeConnections();
+		if (check != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Methode, in die Datenbank relevanten Variablen aufgrundlage des Formulars
+	 * deklariert werden.
+	 */
+	private void deklariereVariablenVTextfeldern() {
+		try {
+			name = stView.getTfName().getText();
+			laenge = Integer.parseInt(stView.getTfLaenge().getText());
+			schwierigkeit = Integer.parseInt(stView.getTfSchwierigkeit().getText());
+			punktzahl = Integer.parseInt(stView.getTfPunktzahl().getText());
+			status = stView.getRbFree().isSelected() ? 'f' : (stView.getRbPremium().isSelected() ? 'p' : ' ');
+			grafik = new File(stView.getTfGrafik().getText());
 		} catch (NumberFormatException e) {
 			istKeineZahl = true;
 		}
 	}
-	
-	//Methode, welche eine Strecke in der Datenbank erstellt
-	private void erstelleStreckeInDB(){
-		if(status == 'p'){
-				typ = "true";
+
+	/**
+	 * Methode, welche eine Strecke in der Datenbank erstellt.
+	 */
+	private void erstelleStreckeInDB() {
+		if (status == 'p') {
+			typ = "true";
+		} else {
+			typ = "false";
 		}
-		String abfrage = "insert into strecke values('" + name + "','" + laenge + "','" + schwierigkeit + "','" + typ + "'," 
-		+ punktzahl + ",null)";
+		String abfrage = "insert into strecke values('" + name + "','" + laenge + "','" + schwierigkeit + "','" + typ
+				+ "'," + punktzahl + ",null)";
 		Datenbankschnittstelle.executeQuery(abfrage);
 		Datenbankschnittstelle.closeConnections();
 	}
-	
-	//Methode, welche die in der Datenbank erstellte Strecke um eine Grafik ergaenzt
-	private void updateKartGrafik(){
+
+	/**
+	 * Methode, welche die in der Datenbank erstellte Strecke um eine Grafik
+	 * ergaenzt.
+	 */
+	private void updateStreckenGrafik() {
 		String updateBild = "update strecke set grafik =? where streckenname ='" + name + "'";
-		Datenbankschnittstelle.uploadBlob(updateBild,grafik);
+		Datenbankschnittstelle.uploadBlob(updateBild, grafik);
 		Datenbankschnittstelle.closeConnections();
 	}
-	
-	//Methode, die das Formular leeren soll
+
+	/**
+	 * Methode, die die erstellte Strecke zum Zwecke des Zwischenspeichern in eine
+	 * Datei schreibt.
+	 */
+	private void speicherStreckeZwischen() {
+		File f = new File("src/Resources/tempStr.dat");
+		if (status == 'p') {
+			typ = "true";
+		} else {
+			typ = "false";
+		}
+		String zeile = name + ";" + laenge + ";" + schwierigkeit + ";" + typ + ";" + punktzahl;
+		try {
+			FileWriter fw = new FileWriter(f, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(zeile);
+			bw.newLine();
+			bw.close();
+			fw.close();
+		} catch (IOException e) {
+			streckeKonnteNichtZwischengespeichertWerdenMeldung();
+		}
+	}
+
+	/**
+	 * Methode, die das Formular leeren soll.
+	 */
 	private void leereFormular() {
 		stView.getTfName().setText("");
 		stView.getTfLaenge().setText("");
@@ -227,8 +394,12 @@ public class StreckeHinzufügenStrg implements ActionListener {
 		stView.getTfGrafik().setText("");
 		stView.getBgStatus().clearSelection();
 	}
-	
-	//Getter fuer View
+
+	/**
+	 * Getter fuer StreckeHinzufügenView.
+	 * 
+	 * @return StreckeHinzufügenView
+	 */
 	public StreckeHinzufügenView getView() {
 		return stView;
 	}
--- src/StreckeHinzufügen/StreckeHinzufügenView.java
+++ src/StreckeHinzufügen/StreckeHinzufügenView.java
@@ -1,10 +1,4 @@
-/**
- @author Sean Cartner
-*/
-
 package StreckeHinzufügen;
-
-import java.awt.EventQueue;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -18,7 +12,13 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.UIManager;
 
-
+/**
+ * Grafische Beutzeroberfläche in Form eines Formulars für den Vorgang
+ * HinzufügenStrecke.
+ * 
+ * @author Sean
+ *
+ */
 public class StreckeHinzufügenView extends JPanel {
 
 	private JPanel frmPixelRacer;
@@ -37,9 +37,6 @@ public class StreckeHinzufügenView extends JPanel {
 	private FileFilter filter;
 	private JLabel lblStrecke;
 
-
-
-
 	/**
 	 * Create the application.
 	 */
@@ -51,177 +48,251 @@ public class StreckeHinzufügenView extends JPanel {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
+
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 		frmPixelRacer = new JPanel();
 		setSize(624, 570);
 		setLayout(null);
-		
+
 		JLabel lblName = new JLabel("Name:");
 		lblName.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblName.setBounds(77, 97, 46, 14);
 		add(lblName);
-		
+
 		JLabel lblLaenge = new JLabel("Länge:");
 		lblLaenge.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblLaenge.setBounds(77, 148, 107, 19);
 		add(lblLaenge);
-		
+
 		JLabel lblSchwierigkeit = new JLabel("Schwierigkeit:");
 		lblSchwierigkeit.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblSchwierigkeit.setBounds(77, 201, 148, 19);
 		add(lblSchwierigkeit);
-		
+
 		JLabel lblPunktzahl = new JLabel("Punktzahl:");
 		lblPunktzahl.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblPunktzahl.setBounds(78, 256, 89, 14);
 		add(lblPunktzahl);
-		
+
 		JLabel lblStatus = new JLabel("Status:");
 		lblStatus.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblStatus.setBounds(77, 307, 46, 14);
 		add(lblStatus);
-		
+
 		textFieldName = new JTextField();
 		textFieldName.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		textFieldName.setColumns(10);
 		textFieldName.setBounds(265, 89, 175, 30);
 		add(textFieldName);
-		
+
 		textFieldLaenge = new JTextField();
 		textFieldLaenge.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		textFieldLaenge.setBounds(265, 142, 175, 30);
 		add(textFieldLaenge);
 		textFieldLaenge.setColumns(10);
-		
+
 		textFieldSchwierigkeit = new JTextField();
 		textFieldSchwierigkeit.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		textFieldSchwierigkeit.setBounds(265, 195, 175, 30);
 		add(textFieldSchwierigkeit);
 		textFieldSchwierigkeit.setColumns(10);
-		
+
 		textFieldPunktzahl = new JTextField();
 		textFieldPunktzahl.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		textFieldPunktzahl.setBounds(265, 248, 175, 30);
 		add(textFieldPunktzahl);
 		textFieldPunktzahl.setColumns(10);
-		
-		rdbtnFree = new JRadioButton("Free");;
+
+		rdbtnFree = new JRadioButton("Free");
+		;
 		rdbtnFree.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		rdbtnFree.setBounds(265, 305, 61, 23);
 		add(rdbtnFree);
-		
+
 		rdbtnPremium = new JRadioButton("Premium");
 		rdbtnPremium.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		rdbtnPremium.setBounds(351, 305, 89, 23);
 		add(rdbtnPremium);
-		
+
 		bgStatus = new ButtonGroup();
 		bgStatus.add(rdbtnFree);
 		bgStatus.add(rdbtnPremium);
-		
+
 		JLabel lblGrafik = new JLabel("Grafik:");
 		lblGrafik.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblGrafik.setBounds(77, 358, 46, 19);
 		add(lblGrafik);
-		
+
 		textFieldGrafik = new JTextField();
 		textFieldGrafik.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		textFieldGrafik.setBounds(265, 347, 175, 30);
 		add(textFieldGrafik);
 		textFieldGrafik.setColumns(10);
-		
+
 		fc = new JFileChooser();
 		filter = new FileNameExtensionFilter("Portable Network Graphics (*.png)", "png");
 
 		btnAuswhlen = new JButton("ausw\u00E4hlen");
 		btnAuswhlen.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnAuswhlen.setBounds(470, 346, 112, 30);
-		
+
 		add(btnAuswhlen);
-		
+
 		btnAbsenden = new JButton("absenden");
 		btnAbsenden.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnAbsenden.setBounds(345, 419, 95, 23);
 		add(btnAbsenden);
-		
+
 		lblStrecke = new JLabel("Strecke Hinzuf\u00FCgen");
 		lblStrecke.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblStrecke.setBounds(78, 30, 205, 30);
 		add(lblStrecke);
-		
+
 		btnAbbrechen = new JButton("abbrechen");
 		btnAbbrechen.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnAbbrechen.setBounds(225, 419, 101, 23);
 		add(btnAbbrechen);
-		
 
 	}
-	
+
+	/**
+	 * Getter für das JPanel.
+	 * 
+	 * @return JPanel
+	 */
 	public JPanel getPanel() {
 		return frmPixelRacer;
 	}
-	
+
+	/**
+	 * Getter für textFieldName.
+	 * 
+	 * @return JTextField
+	 */
 	public JTextField getTfName() {
 		return textFieldName;
 	}
-	
+
+	/**
+	 * Getter für textFieldLaenge.
+	 * 
+	 * @return JTExtField
+	 */
 	public JTextField getTfLaenge() {
 		return textFieldLaenge;
 	}
-	
+
+	/**
+	 * Getter für textFieldSchwierigkeit.
+	 * 
+	 * @return JTextField
+	 */
 	public JTextField getTfSchwierigkeit() {
 		return textFieldSchwierigkeit;
 	}
-	
+
+	/**
+	 * Getter für textFieldPunktzahl.
+	 * 
+	 * @return JTextField
+	 */
 	public JTextField getTfPunktzahl() {
 		return textFieldPunktzahl;
 	}
-	
+
+	/**
+	 * Getter für textFieldGrafik.
+	 * 
+	 * @return JTextField
+	 */
 	public JTextField getTfGrafik() {
 		return textFieldGrafik;
 	}
-	
+
+	/**
+	 * Getter für BtnAuswhlen.
+	 * 
+	 * @return JButton
+	 */
 	public JButton getBtnAuswhlen() {
 		return btnAuswhlen;
 	}
-	
+
+	/**
+	 * Getter für BtnAbsenden.
+	 * 
+	 * @return JButton
+	 */
 	public JButton getBtnAbsenden() {
 		return btnAbsenden;
 	}
-	
+
+	/**
+	 * Getter für BtnAbbrechen.
+	 * 
+	 * @return btnAbbrechen
+	 */
 	public JButton getBtnAbbrechen() {
 		return btnAbbrechen;
 	}
-	
+
+	/**
+	 * Getter für rdbtnFree.
+	 * 
+	 * @return JRadioButton
+	 */
 	public JRadioButton getRbFree() {
 		return rdbtnFree;
 	}
-	
+
+	/**
+	 * Getter für rdbtnPremium.
+	 * 
+	 * @return JRadioButton
+	 */
 	public JRadioButton getRbPremium() {
 		return rdbtnPremium;
 	}
-	
+
+	/**
+	 * Getter für bgStatus.
+	 * 
+	 * @return ButtonGroup
+	 */
 	public ButtonGroup getBgStatus() {
 		return bgStatus;
 	}
-	
+
+	/**
+	 * Getter für FileChooser.
+	 * 
+	 * @return JFileChooser
+	 */
 	public JFileChooser getFileChooser() {
 		return fc;
 	}
-	
+
+	/**
+	 * Getter für FileFilter.
+	 * 
+	 * @return FileFilter
+	 */
 	public FileFilter getFilter() {
 		return filter;
 	}
 
+	/**
+	 * Getter für lblStrecke.
+	 * 
+	 * @return JLabel
+	 */
 	public JLabel getLblStrecke() {
 		return lblStrecke;
 	}
-	
-	
+
 }
